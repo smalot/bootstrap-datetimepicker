b1cfc1b6ef2d11c40c3ee1013a8fac02f4a10945
Gabor Daher <darevish@netpositive.hu>
2013-02-19 13:26:42 +0100

    hungarian locale added

---
+++ js/locales/bootstrap-datetimepicker.hu.js
@@ -0,0 +1,16 @@
+/**
+ * Danish translation for bootstrap-datetimepicker
+ * Christian Pedersen <http://github.com/chripede>
+ */
+;(function($){
+	$.fn.datetimepicker.dates['hu'] = {
+		days: ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"],
+		daysShort: ["Vas", "Hét", "Ked", "Sze", "Csü", "Pén", "Szo", "Vas"],
+		daysMin: ["V", "H", "K", "Sze", "Cs", "P", "Szo", "V"],
+		months: ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"],
+		monthsShort: ["Jan", "Feb", "Már", "Ápr", "Máj", "Jún", "Júl", "Aug", "Sze", "Okt", "Nov", "Dec"],
+		today: "Ma",
+		suffix: [],
+		meridiem: []
+	};
+}(jQuery));
