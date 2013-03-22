743ed8bc2d1206e1f8653922c47680e6a4358736
Simon <contact@freyskeyd.fr>
2013-03-22 14:54:30 +0100

    Available Date

--- js/bootstrap-datetimepicker.js
+++ js/bootstrap-datetimepicker.js
@@ -6,7 +6,7 @@
  * Improvements by Sébastien Malot
  * Project URL : http://www.malot.fr/bootstrap-datetimepicker
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
