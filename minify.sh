c677af076bddc22937a4d16252df234b85710538
Sebastien Malot <sebastien@malot.fr>
2013-10-12 17:29:36 +0200

    normalize css and js files

---
+++ minify.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+java -jar ../yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar css/bootstrap-datetimepicker.css > css/bootstrap-datetimepicker.min.css
+java -jar ../yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar js/bootstrap-datetimepicker.js > js/bootstrap-datetimepicker.min.js
