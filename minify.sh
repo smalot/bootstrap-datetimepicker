7bf822b0a7892f8e91b75d695fb148e31792e67d
krzysztof.zdzanski <krzysztof.zdzanski@home.net.pl>
2014-10-16 11:11:41 +0200

    fix yui-cmpressor path in minify script

--- minify.sh
+++ minify.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar ~/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar css/bootstrap-datetimepicker.css > css/bootstrap-datetimepicker.min.css
-java -jar ~/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar js/bootstrap-datetimepicker.js > js/bootstrap-datetimepicker.min.js
+yui-compressor css/bootstrap-datetimepicker.css > css/bootstrap-datetimepicker.min.css
+yui-compressor js/bootstrap-datetimepicker.js > js/bootstrap-datetimepicker.min.js
