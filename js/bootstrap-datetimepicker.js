4afd9772d0046edbf606533d10a3db86431ed76c
lin23871 <lin23871@163.com>
2015-09-10 19:16:26 +0800

    fixed endDate bug

--- js/bootstrap-datetimepicker.js
+++ js/bootstrap-datetimepicker.js
@@ -753,7 +753,7 @@
         months.slice(0, startMonth + 1).addClass('disabled');
       }
       if (year == endYear) {
-        months.slice(endMonth).addClass('disabled');
+        months.slice(endMonth + 2).addClass('disabled');
       }
 
       html = '';
