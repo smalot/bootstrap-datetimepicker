ae3c2fd29bab7196701a147722544e7e927939ba
Ryan Tankersley <ryan.tankersley@interworks.com>
2016-05-12 09:58:30 -0500

    When all parsing fails, uses date parsing. If that fails, gives the current date

--- tests/suites/formats.js
+++ tests/suites/formats.js
@@ -245,3 +245,11 @@ test('Untrimmed datetime value', patch_date(function(Date){
       .datetimepicker('setValue');
   equal(this.input.val(), '2012-03-05 00:00');
 }));
+
+test('When parsing does not work uses date parsing', function(){
+    this.input
+        .val('8/19/1986')
+        .datetimepicker({format: 'mm/dd/yy HH:ii P'})
+        .datetimepicker('setValue');
+    equal(this.input.val(), '08/19/86 12:00 AM');
+});
